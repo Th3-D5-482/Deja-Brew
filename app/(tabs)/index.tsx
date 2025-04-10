0ae91ad0981ad911ee9fc82bf3ca99ef28d5166e
Megatron482 <davesquare482@gmail.com>
2025-04-10 17:58:39 +0530

    Th3_D5_482

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react';
 import {styles} from '@/styles/style'
+import { Colors } from '@/components/colors'
 
 export default function index() {
   return (
-    <View style = {styles.container}>
-      <Text style ={styles.text}>TabIndex</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={Colors.primary}/>
+      <Text style ={styles.text}>déjà Brew</Text>
     </View>
   )
 }
--- styles/style.js
+++ styles/style.js
@@ -5,8 +5,14 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: Colors.primary,
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingTop: 32,
     },
     text: {
-        color: "#fff"
+        color: "#fff",
+        fontSize: 32,
+        fontWeight: "bold",
+        maxWidth: 80,
     }
 });
