471defb49c989c2fd6cbbe8aae5be8194fccb4be
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-04-17 23:34:51 +0530

    Th3_D5_482

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -34,7 +34,7 @@ export default function index() {
   }
   return (
     <View className="flex-1 px-8 pt-5" style ={{backgroundColor: Colors.primary}}>
-      <ScrollView>
+      <ScrollView contentContainerStyle ={{paddingBottom: 20}} showsVerticalScrollIndicator = {false}>
         <StatusBar backgroundColor={Colors.primary}/>
         <View className='flex flex-row justify-center mb-12 gap-2'>
           <Text className='text-[#877b74] text-[32px]'>déjà</Text>
@@ -44,7 +44,7 @@ export default function index() {
           <Image source={require('@/assets/images/search.png')} className='top-[15px]'/>
           <TextInput placeholder='Browse your favorite coffee ...' placeholderTextColor={"#877b74"} className='ml-15 text-[18px] text-white'/>
         </View>
-        <View className='h-[580px]'>
+        <View className='h-50'>
           <View className='h-50 rounded-xl py-2 px-2 flex flex-row justify-between'>
             <TouchableOpacity onPress={funCappuccino}>
               <Text style ={[{fontSize: 16},{color: cappuccino == 1 ? "#EFE3C8" : "#7d6b65"}]}>Cappuccino</Text>
@@ -59,9 +59,9 @@ export default function index() {
               <Text style ={[{fontSize: 16},{color: espresso == 1 ? "#EFE3C8" : "#7d6b65"}]}>Espresso</Text>
             </TouchableOpacity>
           </View>
-          <View className='grid grid-cols-2 mt-8 h-[520px]'>
-            <View className='w-full h-[300px] flex flex-row'>
-              <View className='w-1/2 border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
+          <View className='grid grid-cols-2 mt-8 h-50 gap-5'>
+            <View className='w-full h-[300px] flex flex-row justify-between'>
+              <View className='w-[48%] border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
                 <Image source={require('@/assets/images/coffee.png')} style ={{width: 120, height: 150}}/>
                 <Text className='mt-2 text-white text-xl'>Cinnamon & Cocoa</Text>
                 <View className='w-full mt-3 h-10 bg-[#463d46] rounded-xl flex flex-row justify-between'>
@@ -69,7 +69,43 @@ export default function index() {
                   <TouchableOpacity className='w-10 h-10 border bg-[#efe3c8] rounded-xl'><Text className='text-5xl px-1'>+</Text></TouchableOpacity>
                 </View>
               </View>
-              <View className='w-1/2 border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
+              <View className='w-[48%] border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
+                <Image source={require('@/assets/images/coffee.png')} style ={{width: 120, height: 150}}/>
+                <Text className='mt-2 text-white text-xl'>Cinnamon & Cocoa</Text>
+                <View className='w-full mt-3 h-10 bg-[#463d46] rounded-xl flex flex-row justify-between'>
+                  <Text className='px-8 py-1 text-white text-2xl font-bold'>$100</Text>
+                  <TouchableOpacity className='w-10 h-10 border bg-[#efe3c8] rounded-xl'><Text className='text-5xl px-1'>+</Text></TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            <View className='w-full h-[300px] flex flex-row justify-between'>
+              <View className='w-[48%] border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
+                <Image source={require('@/assets/images/coffee.png')} style ={{width: 120, height: 150}}/>
+                <Text className='mt-2 text-white text-xl'>Cinnamon & Cocoa</Text>
+                <View className='w-full mt-3 h-10 bg-[#463d46] rounded-xl flex flex-row justify-between'>
+                  <Text className='px-8 py-1 text-white text-2xl font-bold'>$100</Text>
+                  <TouchableOpacity className='w-10 h-10 border bg-[#efe3c8] rounded-xl'><Text className='text-5xl px-1'>+</Text></TouchableOpacity>
+                </View>
+              </View>
+              <View className='w-[48%] border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
+                <Image source={require('@/assets/images/coffee.png')} style ={{width: 120, height: 150}}/>
+                <Text className='mt-2 text-white text-xl'>Cinnamon & Cocoa</Text>
+                <View className='w-full mt-3 h-10 bg-[#463d46] rounded-xl flex flex-row justify-between'>
+                  <Text className='px-8 py-1 text-white text-2xl font-bold'>$100</Text>
+                  <TouchableOpacity className='w-10 h-10 border bg-[#efe3c8] rounded-xl'><Text className='text-5xl px-1'>+</Text></TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            <View className='w-full h-[300px] flex flex-row justify-between'>
+              <View className='w-[48%] border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
+                <Image source={require('@/assets/images/coffee.png')} style ={{width: 120, height: 150}}/>
+                <Text className='mt-2 text-white text-xl'>Cinnamon & Cocoa</Text>
+                <View className='w-full mt-3 h-10 bg-[#463d46] rounded-xl flex flex-row justify-between'>
+                  <Text className='px-8 py-1 text-white text-2xl font-bold'>$100</Text>
+                  <TouchableOpacity className='w-10 h-10 border bg-[#efe3c8] rounded-xl'><Text className='text-5xl px-1'>+</Text></TouchableOpacity>
+                </View>
+              </View>
+              <View className='w-[48%] border border-green h-[300px] bg-[#362c36] px-5 rounded-xl py-5'>
                 <Image source={require('@/assets/images/coffee.png')} style ={{width: 120, height: 150}}/>
                 <Text className='mt-2 text-white text-xl'>Cinnamon & Cocoa</Text>
                 <View className='w-full mt-3 h-10 bg-[#463d46] rounded-xl flex flex-row justify-between'>
