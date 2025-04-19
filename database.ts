3ac3ad54ef72432afa9fc273019250cc61c4cb94
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-04-19 22:44:50 +0530

    Th3_D5_482

--- database.ts
+++ database.ts
@@ -3,36 +3,36 @@ export const drink1 = [
         id: 0,
         name: "Classic Cappuccino",
         price: "4.30",
-        imagePath: "",
+        imagePath: "@/assets/images/classic-cappuccio.png",
     },
     {
         id: 1,
         name: "Caramel Cappuccino",
         price: "4.50",
-        imagePath: "",
+        imagePath: "@/assets/images/classic-cappuccio.png",
     },
     {
         id: 2,
         name: "Vanilla Cappuccino",
         price: "4.50",
-        imagePath: "",
+        imagePath: "@/assets/images/classic-cappuccio.png",
     },
     {
         id: 3,
         name: "HazeInut Cappuccino",
         price: "4.50",
-        imagePath: "",
+        imagePath: "@/assets/images/classic-cappuccio.png",
     },
     {
         id: 4,
         name: "Mocha Cappuccino",
         price: "4.75",
-        imagePath: "",
+        imagePath: "@/assets/images/classic-cappuccio.png",
     },
     {
         id: 5,
         name: "Chocolate Cappuccino",
         price: "4.75",
-        imagePath: "",
+        imagePath: "@/assets/images/classic-cappuccio.png",
     }
 ]
