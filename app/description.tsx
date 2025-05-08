d2c54b5c928c2d86f06c0e4e0c569b578b8f3d00
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-05-09 01:18:33 +0530

    Th3_D5_482

--- app/description.tsx
+++ app/description.tsx
@@ -11,17 +11,26 @@ export default function description() {
         <ScrollView showsVerticalScrollIndicator = {false}>
             <View className='border border-green-900 h-[450px] rounded-xl'>
               <Image source={require("@/assets/images/classic-cappuccino.png")} style={{ width: 400, height: 480, objectFit: 'contain' }} className='mx-auto' />
-                <View className='rounded-full h-12 w-12 border border-red-900 top-[-460px] left-5 p-1'>
+                <View className='rounded-full h-12 w-12 top-[-460px] left-5 p-1'>
                   <TouchableOpacity onPress={() => router.back()}>
                     <Ionicons name='arrow-back' color={Colors.inactiveTab} size ={32}></Ionicons>
                   </TouchableOpacity>
                 </View>
             </View>
-            <View className='border border-red-900 mt-[30px] h-[80px] flex flex-row'>
-              <View className='w-[80%] border border-green-900 h-[80px]'>
+            <View className='mt-[30px] h-[80px] flex flex-row'>
+              <View className='w-[80%] h-[80px]'>
                 <Text className='text-white text-2xl font-bold'>{name}</Text>
+                <View className='flex flex-row mt-2 '>
+                  <View className='w-[70%] pt-2'>
+                    <Text className='text-white text-xl'>Drillzed with Caramel</Text>
+                  </View>
+                  <View className='w-[30%] p-2 flex flex-row'>
+                    <Ionicons name ="star" size ={24} color ={Colors.inactiveTab}/>
+                    <Text className='text-white text-xl ml-2'>3.5</Text>
+                  </View>
+                </View>
               </View>
-              <View className='w-[20%] border border-green-900 h-[80px] p-4'>
+              <View className='w-[20%] h-[80px] p-4'>
                 <Ionicons name='heart' size={42} color ={Colors.inactiveTab}/>
               </View>
             </View>
